a27d9bbe1188b2ccb652bd65d4befae697b66d2b
JoshHug <hug@cs.berkeley.edu>
2015-09-01 19:09:57 -0700

    modified name of lab 3 to include reporters

--- assets/schedule.js
+++ assets/schedule.js
@@ -72,7 +72,7 @@ cs10.week2 = {
     lect1: lect('Procedures', 'https://docs.google.com/presentation/d/1_lKWy3HeznujFDTeRfffoGSAkvVEPqzpNvKVM2jkC0c/pub'),
     lab1: lab('Build Your Own Blocks', 'berkeley_bjc/intro_pair/2-loops-variables.topic'),
     lect2: lect('Numbers and Abstraction', 'L03 - Abstraction II'),
-    lab2: lab('Conditionals', 'berkeley_bjc/intro_pair/3-conditionals.topic', true),
+    lab2: lab('Conditionals and Reporters', 'berkeley_bjc/intro_pair/3-conditionals.topic', true),
     disc1: disc('Anatomy of a Computer and the Power of Binary'),
     hw: hw('HW1: Word Guessing', 'Fri 9/11', 'assignments/6644471', hw1Spec)
 };
