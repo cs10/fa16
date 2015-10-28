b75c3402f1d8522064593455f172e8cee1df7801
JoshHug <hug@cs.berkeley.edu>
2015-10-28 07:58:53 -0700

    updated page with slides for AI

--- assets/schedule.js
+++ assets/schedule.js
@@ -208,7 +208,7 @@ cs10.week8 = {
     disc1: disc('More Recursion', true),
     lab1: lab('Recursive Reporters I', 'berkeley_bjc/recur/recursive-reporters-part1.topic', true),
     lect1: lect('Recursion II', 'https://docs.google.com/presentation/d/1IzvyATwwmue57YXuGLXevOyTLNgepV0xH8VaiGGHrYw/pub?start=false&loop=false&delayms=3000'),
-    lect2: lect('Privacy', 'L17 - Privacy'),
+    lect2: lect('Privacy', 'L13 - Privacy'),
     lab2: lab('Project Work Session'),
     hw: hw('Midterm Project Proposals', 'Fri 10/16', 'assignments/6644519', mtProjSpec)
 };
@@ -255,7 +255,7 @@ cs10.week10 = {
     disc1: disc('Midterm Debrief'),
     lab1: lab('Online <span class="snap">snap</span> Midterm'),
     lect1: lect('Social Implications', 'https://docs.google.com/presentation/d/1a429t0fmkv98dXn1TJGCyxOAiwHiGY7_fZ9ctIRPFD4/edit?usp=sharing'),
-    lect2: lect('AI'),
+    lect2: lect('AI', 'L17 - AI'),
     lab2: lab('Project Work Session'),
     hw: [        
         hw('Midterm Project', '11/1', 'assignments/6644475')
