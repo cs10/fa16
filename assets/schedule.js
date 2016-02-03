1cd744ba4ac91ab4dc64632ce7aec96cc6bc54aa
Adam Kuphaldt <ak3331@gmail.com>
2016-02-03 14:58:06 -0800

    Adding discussion 2 materials

--- assets/schedule.js
+++ assets/schedule.js
@@ -78,7 +78,7 @@ cs10.week2 = {
     lab1: lab('Build Your Own Blocks', 'berkeley_bjc/intro_pair/2-loops-variables.topic'),
     lect2: lect('Numbers and Abstraction', 'https://docs.google.com/presentation/d/1TVXERNz4ude_YYsPccwwirFWxXQXSs2ex-cccHytfzo/edit?usp=sharing'), //'https://docs.google.com/presentation/d/1zHEl4C_jLGFVFznWPMWhDYFtcLENUU3_DubOb_H5UZs/pub'),
     lab2: lab('Conditionals and Reporters', 'berkeley_bjc/intro_pair/3-conditionals.topic', true),
-    disc1: disc('Anatomy of a Computer and the Power of Binary'),
+    disc1: disc('Anatomy of a Computer and the Power of Binary', true),
     hw: hw('HW1: Word Guessing', '2/3', 'assignments/7259694', hw1Spec)
 };
 
