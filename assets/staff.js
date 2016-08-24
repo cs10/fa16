0de61f67c9e596560da1f21e24492552c00b2d9b
carlosflrs <carlos.flrsarlln@gmail.com>
2016-08-24 12:01:51 -0700

    added my bio, cleaned up staff.js

--- assets/staff.js
+++ assets/staff.js
@@ -11,7 +11,6 @@ var DanGarcia = {
     office: '777 Soda, (510) 517-4041'
 };
 
-
 // TAs
 
 var adamK = {
@@ -19,7 +18,8 @@ var adamK = {
     img: 'Sp14/AdamKuphaldt.jpg',
     imgSrc: 'AdamKuphaldt.jpg',
     email: 'adam@cs10.org',
-    web: 'https://linkedin.com/in/adam-kuphaldt-16a33768' };
+    web: 'https://linkedin.com/in/adam-kuphaldt-16a33768'
+};
 
 var rachelH = {
     name: 'Head TA Rachel Huang',
@@ -28,61 +28,42 @@ var rachelH = {
     email: 'rachel@cs10.org'
 };
 
-var alexM = {
-    name: 'TA Alex McKinney',
-    img: 'Sp14/AlexMcKinney.jpg',
-    imgSrc: 'AlexMcKinney.jpg',
-    email: 'alex@cs10.org'};
-    
-var amrutaY = {
-    name: 'TA Amruta Yelamanchili',
-    img: 'Sp14/AmrutaYelamanchili.jpg',
-    imgSrc: 'AmrutaYelamanchili.jpg',
-    email: 'amruta@cs10.org'};
-
-
-var andyS = {
-    name: 'Development TA Andy Schmitt',
-    img: 'Sp14/AndrewSchmitt.jpg',
-    imgSrc: 'AndrewSchmitt.jpg',
-    email: 'andy@cs10.org'};
-    
 var aranyU = {
     name: 'TA Arany Uthayakumar',
     img: 'Sp14/AranyUthayakumar.jpg',
     imgSrc: 'AranyUthayakumar.jpg',
     // bio: 'AranyBio.txt',
-    email: 'arany@cs10.org' };
+    email: 'arany@cs10.org' 
+};
 
 var carlosF = {
     name: 'TA Carlos Flores',
     img: 'Sp14/CarlosFlores.jpg',
+    bio: 'CarlosBio.txt',
     imgSrc: 'CarlosFlores.jpg',
-    email: 'carlos@cs10.org' };
+    email: 'carlos@cs10.org'
+};
     
 var christianL = {
     name: 'TA Christian Lista-Nicoloso',
     img: 'NPY3D.jpg',
     imgSrc: 'NPY3D.jpg',
-    email: 'christian@cs10.org' };
+    email: 'christian@cs10.org'
+};
     
-var claireW = {
-    name: 'TA Claire Watanabe',
-    img: 'Fa13/ClaireWatanabe.jpg',
-    imgSrc: 'ClaireWatanabe.jpg',
-    email: 'claireleilani@berkeley.edu'};
-
 var elizabethS = {
     name: 'TA Elizabeth Steger',
     img: 'sp16/ElizabethSteger.jpg',
     imgSrc: 'ElizabethSteger.jpg',
-    email: 'lizzy@cs10.org'};
+    email: 'lizzy@cs10.org'
+};
 
 var erikD = {
     name: 'TA Erik Dahlquist',
     img: 'Sp15/ErikDahlquist.jpg',
     imgSrc: 'ErikDahlquist.jpg',
-    email: 'erik@cs10.org' };
+    email: 'erik@cs10.org' 
+};
 
 var jannaG = {
     name: 'TA Janna Golden',
@@ -95,14 +76,9 @@ var jobelV = {
     name: 'TA Jobel Vecino',
     img: 'Fa13/JobelVecino.jpg',
     imgSrc: 'JobelVecino.jpg',
-    email: 'jobel@cs10.org' };
+    email: 'jobel@cs10.org' 
+};
 
-var josephC = {
-    name: 'TA Joseph Cawthorne',
-    img: 'Fa13/JosephCawthorne.jpg',
-    imgSrc: 'JosephCawthorne.jpg',
-    email: 'joseph@cs10.org' };
-    
 var laraM = {
     name: 'TA Lara McConnaughey',
     img: 'Su15/LaraMcConnaughey.jpg',
@@ -121,13 +97,6 @@ var stevenT = {
     email: 'steven@cs10.org'
 };
 
-var victoriaS = {
-    name: 'Development TA Victoria Shi',
-    img: 'Fa13/VictoriaShi.jpg',
-    imgSrc: 'VictoriaShi.jpg',
-    // bio: 'VictoriaBio.txt',
-    email: 'victoria@cs10.org' };
-    
 var yifatA = {
   name: 'TA Yifat Amir',
   img: 'sp16/YifatAmir.jpg',
@@ -144,17 +113,6 @@ var williamT = {
 };
 
 // Readers
-
-var katherineM = {
-    name: 'Reader Katherine McGauley',
-    img: 'Su15/KatherineMcGauley.jpg',
-    imgSrc: 'KatherineMcGauley.jpg' };
-
-var lucasK = {
-    name: 'Reader Lucas Karahadian',
-    img: 'NPY3D.jpg',
-    imgSrc: 'NPY3D.jpg',
-};
 
 var meghnaD = {
     name: 'Reader Meghna Dasgupta',
@@ -195,8 +153,6 @@ var noahJ = {
 
 var inst = [ DanGarcia ];
 var tas = [ adamK, rachelH, aranyU, carlosF, christianL, elizabethS, erikD, jannaG, jobelV, laraM, williamT, yifatA, stevenT ];
-
-//var readers = [ jobelV, katherineM, laraM ]; todo: Update this once readers are hired!
 var readers = [ addisonH, matthewS, meghnaD, noahJ, samuelS ];
 
 // If you need to add a new SECTION add it to this object.
@@ -224,7 +180,6 @@ function buildPerson(data, width) {
     var imgPathBase, imgPath, cls, elm, crazyPath;
     // Given a JSON object build a div that contains all the person's info
     // width is used to control how many are on a row on the page.
-
     // Build data objects for very simple cases with nothing special.
     if (data.constructor === String) {
         data = baseObj(data);
@@ -237,12 +192,10 @@ function buildPerson(data, width) {
         imgPathBase = '';
     }
     imgPath = imgPathBase + '/resources/images/small/' + data.imgSrc;
-
     // Date Hacks for fun!
     if (Date().substr(4, 6) == 'Apr 01') {
         imgPath = 'https://www.cs.berkeley.edu/~ddgarcia/i/DanGarciaUCBFaculty2004Eyebrow_320.jpg';
     }
-
     // Create a div with this person's data, setting a class for width
     // Col-md- is based on standard bootstrap classes, md-20 is my own addition.
     cls = 'col-md-' + (width === 5 ? '20' : Math.floor(12/width));
@@ -250,7 +203,6 @@ function buildPerson(data, width) {
     if (data.img) {
         elm += '<a href="' + imgPathBase + '/resources/images/' + data.img + '">';
     }
-
     elm += '<img class="staff" align="center" ';
     elm += 'alt=" Staff Photo: ' + data.name + '" title="' + data.name + '" src="';
     elm += imgPath + '"';
@@ -321,6 +273,7 @@ function addLoadEvent(func) {
 function crazyImage(image, crazyPath) {
     image.src = crazyPath;
 }
+
 function normalImage(image, normalPath) {
     image.src = normalPath;
 }
