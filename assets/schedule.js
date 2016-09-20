d4340176df6e07069ea5ac9c3382e9e8144a8e9f
Steven Traversi <straversi@berkeley.edu>
2016-09-19 23:31:23 -0700

    Link new hw3 spec

--- assets/schedule.js
+++ assets/schedule.js
@@ -27,7 +27,7 @@ function lectureURL(googId) {
 // CONSTANTS:
 var hw1Spec         = docs('1ybuatsrUMCI-tHHRPwxUpGoZoGxk_SEiGEArocQtIlI')
     hw2Spec         = docs('1C5d7zYwubcDUDDFn_x6NFpUrT2Ldum3ElnYhsgJ4fMo')
-    hw3Spec         = docs('1z-DZD3ya3Uxp9WkkmqF838Giq_VEjtdmXNI0dVzU5sg')
+    hw3Spec         = docs('1hFUynrAcREKc6EVpunCI0T6Xlwrc5pBvx8bVv6DKxmE')
     mtProjSpec      = docs('1fFLorzOLOIO4WXzIxllMBACuEvMWZx-ykeLO-rsAIjc')
     exploreSpec     = docs('1i79rb0BL0FJV54cy7s-VC1wa7bUwlfVUT5xFN_o2EDc')
     finalProjSpec   = docs('1eoPoHWad-yBrsIFKZx_AltKD4xlcvVPEdGxIviQQMGM')
